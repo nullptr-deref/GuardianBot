7ee9ac84867cabad61cbb481a2e8523391b7bdc3
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2020-11-24 16:53:38 +0300

    Added array class for comfortable work with data sets

---
+++ Array.hpp
@@ -0,0 +1,71 @@
+template <typename T>
+class Array
+{
+public:
+    Array() = default;
+    Array(size_t s) : size(s)
+    {
+        data = new T[size];
+    }
+    explicit Array(T *dat, size_t s) : size(s)
+    {
+        data = new T[size];
+        for (unsigned int i = 0; i < size; i++)
+        {
+            data[i] = dat[i];
+        }
+    }
+
+    Array(const Array &other)
+    {
+        this->size = other.size;
+        this->data = new T[size];
+
+        for (unsigned int i = 0; i < size; i++)
+        {
+            data[i] = other[i];
+        }
+    }
+
+    T &operator[](size_t index)
+    {
+        if (index >= size || index < 0) throw std::invalid_argument("Attempt to access out-of-range array element.");
+
+        return data[index];
+    }
+
+    const T &operator[](size_t index) const
+    {
+        if (index >= size || index < 0) throw std::invalid_argument("Attempt to access out-of-range array element.");
+
+        return data[index];
+    }
+
+    Array &operator=(const Array &other)
+    {
+        if (nullptr != data)
+        {
+            delete[] data;
+            data = nullptr;
+        }
+
+        this->size = other.size;
+        this->data = new T[size];
+
+        for (unsigned int i = 0; i < size; i++)
+        {
+            data[i] = other[i];
+        }
+
+        return *this;
+    }
+
+    ~Array()
+    {
+        delete[] data;
+        data = nullptr;
+    }
+
+    unsigned int size = 0;
+    T *data = nullptr;
+};
--- main.cpp
+++ main.cpp
@@ -7,16 +7,21 @@
 
 #include <librealsense2/rs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/dnn.hpp>
 
 int main()
 {
     rs2::context ctx;
+
     cv::namedWindow("Main window", cv::WindowFlags::WINDOW_NORMAL);
+
     auto list = ctx.query_devices();
-    if (list.size() == 0) {
+    if (list.size() == 0)
+    {
         std::cerr << "No devices connected.\n";
         return 0;
     }
+    
     rs2::device dev = list.front();
     return 0;
 }
