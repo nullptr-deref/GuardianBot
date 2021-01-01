863886533feb60336c53124350a1c25a77a2c27c
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2021-01-01 11:06:40 +0300

    Realised event subscription

--- events/Listener.cpp
+++ events/Listener.cpp
@@ -4,16 +4,35 @@ namespace events
 {
     void Listener::listenTo(const Emitter &em)
     {
-        this->m_qPtr = em.provideQueueLink();
+        this->m_queue = em.provideQueueLink();
     }
 
     Event &Listener::peekFirstEvent() const
     {
-        return m_qPtr->peekFirst();
+        return m_queue->peekFirst();
     }
 
     Event &Listener::peekLatestEvent() const
     {
-        return m_qPtr->peekLatest();
+        return m_queue->peekLatest();
+    }
+
+    void Listener::subscribeForEvent(const Event &ev, const std::function<void()> &callback)
+    {
+        m_callbackRouter.insert({ ev.is(), callback });
+    }
+
+    bool Listener::processEventIfSubscribed()
+    {
+        Event & ev = m_queue->peekFirst();
+        if (auto it = m_callbackRouter.find(ev.is()); it != m_callbackRouter.end())
+        {
+            it->second();
+            m_queue->dequeue();
+
+            return true;
+        }
+
+        return false;
     }
 }
--- events/Listener.hpp
+++ events/Listener.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
+#include <functional>
+#include <map>
+
 #include "Emitter.hpp"
 
 namespace events
 {
     class Listener
     {
-        std::shared_ptr<Queue> m_qPtr = nullptr;
+        std::shared_ptr<Queue> m_queue = nullptr;
+        std::map<std::string, std::function<void(void)>> m_callbackRouter;
     public:
         Listener() = default;
 
+        void listenTo(const Emitter &em);
+        void subscribeForEvent(const Event &ev, const std::function<void()> &callback);
+        bool processEventIfSubscribed();
         Event &peekFirstEvent() const;
         Event &peekLatestEvent() const;
-        void listenTo(const Emitter &em);
     };
 }
