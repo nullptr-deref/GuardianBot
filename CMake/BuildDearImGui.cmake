622f6d42053cf9b32b2907bfa486e77a2b5e2c12
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2021-01-03 13:17:02 +0300

    Integrated DearImGui into build system

---
+++ CMake/BuildDearImGui.cmake
@@ -0,0 +1,18 @@
+project(DearImGui LANGUAGES CXX)
+
+set(DearImGui_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/imgui;${CMAKE_SOURCE_DIR}/imgui/backends")
+# I'm building imgui for using it with glfw and OpenGL3 so I'm building suitable backends for it
+set(DearImGui_BACKEND_SRCS "${CMAKE_SOURCE_DIR}/imgui/backends/imgui_impl_glfw.cpp;${CMAKE_SOURCE_DIR}/imgui/backends/imgui_impl_opengl3.cpp")
+file(GLOB DearImGui_SRCS "imgui/*.cpp")
+
+add_library(dearimgui STATIC ${DearImGui_BACKEND_SRCS} ${DearImGui_SRCS})
+target_include_directories(dearimgui PRIVATE
+    ${DearImGui_INCLUDE_DIRS}
+    ${glfw_INCLUDE_DIRS}
+    ${glew_INCLUDE_DIRS}
+)
+target_compile_definitions(dearimgui
+    PRIVATE
+    IMGUI_IMPL_OPENGL_LOADER_GLEW
+    GLEW_STATIC
+)
