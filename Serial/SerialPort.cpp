7c9a81d8f8e483a114f6cb640724d027092e66be
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2021-02-23 16:16:22 +0300

    Had to rewrite serial class in order to use it with smart pointers
    Because of incomplete type conflict smart pointer could not use deleter on it, so I had to sacrifice cross-platform.

--- Serial/SerialPort.cpp
+++ Serial/SerialPort.cpp
@@ -1,38 +1,78 @@
 #include "SerialPort.hpp"
 
-#include "SerialPortImpl.cpp"
+#include <stdexcept>
 
-SerialPort::SerialPort(const std::string &portName, int mode, uint baudrate)
-{
-	pImpl = std::make_unique<SerialPortImpl>(portName, mode, baudrate);
-}
+SerialPort::SerialPort(const std::string &portName, int mode, uint32_t baudrate)
+: name(portName), mode(mode), baudrate(baudrate) {}
 
-SerialPort::SerialPort(const std::string &portName, unsigned long mode, uint baudrate)
-{
-	pImpl = std::make_unique<SerialPortImpl>(portName, mode, baudrate);
-}
+SerialPort::SerialPort(const std::string &portName, unsigned long mode, uint32_t baudrate)
+: name(portName), mode(mode), baudrate(baudrate) {}
 
 void SerialPort::open()
 {
-	pImpl->open();
+	m_hCom = CreateFileA(name.c_str(), mode, false, nullptr, CREATE_NEW, OPEN_EXISTING, nullptr);
+
+	if (INVALID_HANDLE_VALUE == m_hCom) throw std::runtime_error("Cannot connect to serial port [" + name + ']');
+
+	DCB m_serialParams { 0 };
+	m_serialParams.DCBlength = sizeof(m_serialParams);
+
+	const bool currComStatusRetrieved = GetCommState(m_hCom, &m_serialParams);
+
+	if (!currComStatusRetrieved) throw std::runtime_error("Could not retrieve serial port status.");
+
+	m_serialParams.BaudRate = baudrate;
+	m_serialParams.fBinary = true;
+	m_serialParams.fParity = true;
+	m_serialParams.StopBits = ONESTOPBIT;
+	m_serialParams.Parity = PARITY_NONE;
+
+	SetCommState(m_hCom, &m_serialParams);
+
+	COMMTIMEOUTS timeouts = { 0 };
+	timeouts.ReadIntervalTimeout = 50;
+	timeouts.ReadTotalTimeoutConstant = 50;
+	timeouts.ReadTotalTimeoutMultiplier = 50;
+	timeouts.WriteTotalTimeoutConstant = 50;
+	timeouts.WriteTotalTimeoutMultiplier = 10;
+
+	SetCommTimeouts(m_hCom, &timeouts);
 }
 
 void SerialPort::close()
 {
-	pImpl->close();
+	bool isClosed = CloseHandle(m_hCom);
+    if (!isClosed) throw std::runtime_error("Could not close the serial port.");
 }
 
-void SerialPort::write(const char *data, uint count)
+void SerialPort::write(const char *data, uint32_t count)
 {
-	pImpl->write(data, count);
+	DWORD bytesWritten = 0;
+	const bool isWritten = WriteFile(m_hCom, data, count, &bytesWritten, nullptr);
+	if (!isWritten) throw std::runtime_error("Could not write to serial port.");
 }
 
 const char* SerialPort::read()
 {
-	return pImpl->read();
+	char buf[64];
+	DWORD bytesRead = 0;
+	const bool isRead = ReadFile(m_hCom, buf, 64, &bytesRead, nullptr);
+	if (!isRead) throw std::runtime_error("Could not read from the serial port.");
+
+	return buf;
 }
 
 std::vector<std::string> SerialPort::queryAvailable()
 {
-	return SerialPortImpl::queryAvailable();
+	std::vector<std::string> availablePorts;
+	const size_t BUF_LENGTH = 1024u;
+	char *buf = new char[BUF_LENGTH];
+	for (size_t i = 0; i < 256; i++)
+	{
+		const std::string portName = "COM" + std::to_string(i);
+		DWORD res = QueryDosDevice(portName.c_str(), buf, BUF_LENGTH);
+		if (res > 0) availablePorts.push_back(portName);
+	}
+
+	return availablePorts;
 }
--- Serial/SerialPort.hpp
+++ Serial/SerialPort.hpp
@@ -16,36 +16,24 @@ namespace SerialMode
 
 #endif
 
-#ifdef __unix__
-
-#include <fnctl.h>
-
-namespace SerialMode
-{
-    int Read = O_RDONLY;
-    int Write = O_WRONLY;
-    int ReadWrite = O_RDWR;
-}
-
-#endif
-
-class SerialPortImpl;
-
-typedef unsigned int uint;
 
 class SerialPort
 {
 public:
-    SerialPort(const std::string &portName, int mode = SerialMode::ReadWrite, uint baudrate = 9600u);
-    SerialPort(const std::string &portName, unsigned long mode = SerialMode::ReadWrite, uint baudrate = 9600u);
+    SerialPort(const std::string &portName, int mode = SerialMode::ReadWrite, uint32_t baudrate = 9600u);
+    SerialPort(const std::string &portName, unsigned long mode = SerialMode::ReadWrite, uint32_t baudrate = 9600u);
 
     void open();
     void close();
     const char *read();
-    void write(const char *data, uint count);
+    void write(const char *data, uint32_t count);
     static std::vector<std::string> queryAvailable();
 
 private:
-    std::unique_ptr<SerialPortImpl> pImpl = nullptr;
+    // std::unique_ptr<SerialPortImpl> pImpl = nullptr;
     std::string name;
+    uint32_t baudrate;
+    unsigned long mode;
+    HANDLE m_hCom;
+    DCB m_serialParams;
 };
