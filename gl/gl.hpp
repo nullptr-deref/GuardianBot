8f0ff4964ccd9d04757d6676f089c995e58c4ffb
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2021-06-08 23:54:29 +0300

    Moved all necessary includes into single file

---
+++ gl/gl.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "glstuff.hpp"
+#include "IndexBuffer.hpp"
+#include "Program.hpp"
+#include "Shader.hpp"
+#include "Texture.hpp"
+#include "VertexArray.hpp"
+#include "VertexBuffer.hpp"
